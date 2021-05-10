e6352fb30b7aa07d20a09c1e1860bf5bb017f573
lowweb <looper@MacBook-Pro-looper.local>
2021-05-11 01:36:52 +1000

    paralax

--- src/blocks/advant/advant.js
+++ src/blocks/advant/advant.js
@@ -4,41 +4,51 @@ let field = document.querySelector(".advant");
 
 
 
-if (field) {
-  let cardLocation = field.getBoundingClientRect();
+if (cards) {
+  
   field.onmouseout = ()=>{
 
     for (i = 0; i < cards.length; ++i) {
-      cards[i].style.transform = "rotateY(0) rotateX(0) scale(1)"
+      cards[i].style.transform = "rotateY(0) rotateX(0)"
     }
   }
+  let oldX = 0
+  let oldY = 0
+
   field.onmousemove = (e)=>{
-  
-    let x = ((e.pageX - cardLocation.left) / cardLocation.width) * 2 - 1
-    let y = ((e.pageY - cardLocation.top) / cardLocation.height) * 2 -1
-    
-  // console.log ('x='+x)
-  // console.log ('y=' +y)
-    let rotateX = y*15;
-    let rotateY =  x*15;
-  
-    // if (rotateY > 10) {
-    //   rotateY = 10;
-    // }
-    // if (rotateY < -5) {
-    //   rotateY = -5;
-    // }
-  
-    // if (rotateX < 348) {
-    //   rotateX = 348
-    // }
-  
-    // if (rotateX > 370) {
-    //   rotateX = 370
-    // }
-    for (i = 0; i < cards.length; ++i) {
-      cards[i].style.transform = "rotateY("+rotateY+"deg) rotateX("+rotateX+"deg)"
+
+    let x = e.pageX
+    let y = e.pageY
+
+    if ( x > oldX) {
+      if (y > oldY) {
+        for (i = 0; i < cards.length; ++i) {
+          cards[i].style.transform = "rotateY("+ -10 +"deg) rotateX("+ -15 +"deg)"
+        }
+      }
+      if (y < oldY) {
+        for (i = 0; i < cards.length; ++i) {
+          cards[i].style.transform = "rotateY("+ -10 +"deg) rotateX("+ 15 +"deg)"
+        }
+      }
     }
-  
+    if ( x < oldX) {
+      if (y > oldY) {
+        for (i = 0; i < cards.length; ++i) {
+          cards[i].style.transform = "rotateY("+ 10 +"deg) rotateX("+ -15 +"deg)"
+        }
+      }
+      if (y < oldY) {
+        for (i = 0; i < cards.length; ++i) {
+          cards[i].style.transform = "rotateY("+ 10 +"deg) rotateX("+ 15 +"deg)"
+        }
+      }
+    }
+
+
+    oldX = x;
+    oldY = y;
+
   }
+ 
 }
